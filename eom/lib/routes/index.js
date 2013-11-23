1c6d231c9185887e59b93bd130783fbca6001147
dinght <match.ding@gmail.com>
2013-11-23 15:51:47 +0800

    view

--- eom/lib/routes/index.js
+++ eom/lib/routes/index.js
@@ -1,12 +1,7 @@
 var LoginUser = require("../bizz/loginUser");
 
 exports.index = function(req, res) {
-	var hostList = host.getAllDomain();
-	var h = hostList[0].getHosts()[0];
-	console.log(h);
-	res.render('index', {
-		domains : hostList
-	});
+	res.render('index');
 
 };
 
